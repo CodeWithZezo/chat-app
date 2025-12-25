c234b113138ffca7f48a127cfcf5c8722cbf6452
Zeeshan Shami <zeeshanshami561@gmail.com>
2025-12-25 23:23:08 +0500

    commenst added

--- server/server.js
+++ server/server.js
@@ -6,19 +6,19 @@ const cors = require('cors');
 app.use(cors(
   {origin: process.env.FRONTEND_URL, credentials: true}
 ));
-
+// Importing Routes
 const authRoutes = require('./routes/auth.route');
 const messageRoutes = require('./routes/message.route.js');
 const userRoutes = require('./routes/user.route.js');
-
+// Connect to Database
 const connectDB = require('./config/db');
 dotenv.config();
-
+//port
 const PORT = process.env.PORT || 3000; 
-
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
-
+// Routes Middleware
 app.use('/api/auth',authRoutes)
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
